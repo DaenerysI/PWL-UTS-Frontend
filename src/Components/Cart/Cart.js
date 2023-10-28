bc61bd660d0b214d7c21baf505fce406048a0ee8
muhamad.120140040@student.itera.ac.id <muhamad.120140040@student.itera.ac.id>
2023-10-28 23:08:15 +0700

    jadi

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,8 +1,7 @@
-// Cart.js
 import React from "react";
 import CartProduct from "./CartProduct";
 
-const Cart = ({ cartProducts, onRemoveFromCart, onDecreaseQuantity }) => {
+const Cart = ({ cartProducts, onRemoveFromCart }) => {
   return (
     <div>
       <h2>Your Cart</h2>
